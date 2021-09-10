50170f00629cc1a507f1b4a2d9211ed812b624a9
Osipov Sasha <sashaosipov753@gmail.com>
2021-09-10 21:58:23 +0300

    third leson

--- script/script.js
+++ script/script.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const formAnswers = document.querySelector('#formAnswers');
     const nextButton = document.querySelector('#next');
     const prevButton = document.querySelector('#prev');
+    const sendBtn = document.querySelector('#send');
 
     let answerImg;
     let answerImgDOM;
@@ -14,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let answerType;
     let formAndswersImgSrc;
     let formAndswersTitle;
-    
+
     let answerCount = 0;
     let cartCount = 0;
 
@@ -39,7 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const renderAnswer = () => {
         const answerItem = document.querySelectorAll('.answers-item');
-        
+
 
         answerItem.forEach(elem => {
             elem.addEventListener('click', () => {
@@ -57,69 +58,155 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 formAndswersImgSrc = document.querySelector('.form-answers-title');
                 formAndswersTitle = document.querySelector('.form-answers-img-src');
-                
 
-                if (!formAndswersImgSrc || !formAndswersTitle){
+
+                if (!formAndswersImgSrc || !formAndswersTitle) {
                     // formAnswers.after(answerTitleDOM);
                     // formAnswers.after(answerImgDOM);
-                }else if(formAndswersImgSrc || formAndswersTitle){
+                } else if (formAndswersImgSrc || formAndswersTitle) {
                     // formAndswersTitle.textContent = answerTitle;
                     // formAndswersImgSrc.textContent = answerImg;
-                }                
+                }
             });
         });
     };
 
-    const renderQuestions = (imgUrl, title, type) => {
-        formAnswers.innerHTML += `
-            <div class="answers-item d-flex flex-column">
-                <input type="${type}" id="answerItem${cartCount}" name="answer" class="d-none">
-                <label for="answerItem${cartCount}" class="d-flex flex-column justify-content-between">
-                    <img class="answerImg" src="${imgUrl}" alt="burger">
-                    <span>${title}</span>
-                </label>
-            </div>
-        `;
-        cartCount += 1;
+
+    const isNumericInput = (event) => {
+        const key = event.keyCode;
+        return ((key >= 48 && key <= 57) || // Allow number line
+            (key >= 96 && key <= 105) // Allow number pad
+        );
     };
 
+    const isModifierKey = (event) => {
+        const key = event.keyCode;
+        return (event.shiftKey === true || key === 35 || key === 36) || // Allow Shift, Home, End
+            (key === 8 || key === 9 || key === 13 || key === 46) || // Allow Backspace, Tab, Enter, Delete
+            (key > 36 && key < 41) || // Allow left, up, right, down
+            (
+                // Allow Ctrl/Command + A,C,V,X,Z
+                (event.ctrlKey === true || event.metaKey === true) &&
+                (key === 65 || key === 67 || key === 86 || key === 88 || key === 90)
+            )
+    };
+
+    const enforceFormat = (event) => {
+        // Input must be of a valid number format or a modifier key, and not longer than ten digits
+        if (!isNumericInput(event) && !isModifierKey(event)) {
+            event.preventDefault();
+        }
+    };
+
+    const formatToPhone = (event) => {
+        if (isModifierKey(event)) { return; }
+
+        const input = event.target.value.replace(/\D/g, '').substring(0, 10); // First ten digits of input only
+        const areaCode = input.substring(0, 3);
+        const middle = input.substring(3, 6);
+        const last = input.substring(6, 10);
+
+        if (input.length > 6) { event.target.value = `(${areaCode}) ${middle} - ${last}`; }
+        else if (input.length > 3) { event.target.value = `(${areaCode}) ${middle}`; }
+        else if (input.length > 0) { event.target.value = `(${areaCode}`; }
+    };
 
     getGoods((data) => {
         const questions = data.questions;
+        let obj = {};
+
+        const renderQuestions = (imgUrl, title, type) => {
+            formAnswers.innerHTML += `
+                <div class="answers-item d-flex flex-column">
+                    <input type="${type}" id="answerItem${cartCount}" name="answer" class="d-none" value='${title}' ${checkedQuestion(title)}>
+                    <label for="answerItem${cartCount}" class="d-flex flex-column justify-content-between">
+                        <img class="answerImg" src="${imgUrl}" alt="burger">
+                        <span>${title}</span>
+                    </label>
+                </div>
+            `;
+            cartCount += 1;
+        };
+
+        const checkedQuestion = (title) => {
+            for (const [key, value] of Object.entries(obj)) {
+                if (title == value) { return 'checked' }
+            }
+        };
+
+        const chechAnswer = () => {
+            const inputs = [...formAnswers.elements].filter((input) => input.checked);
+
+            inputs.forEach((input, index) => {
+                obj[`${index}_${questions[answerCount].question}`] = input.value;
+                console.log(obj)
+            })
+        };
 
         const playTest = (index) => {
             questionTitle.textContent = questions[index].question;
+            formAnswers.innerHTML = '';
+
             questions[index].answers.forEach(element => {
-                renderQuestions(element.url, element.title, questions[index].type);
+                renderQuestions(element.url, element.title, questions[index].type, index);
             });
-      
+
             renderAnswer();
         };
+
+        const checkIsLast = () => {
+            if (answerCount == questions.length) {
+                formAnswers.innerHTML = `
+                <label for="phone">Enter your phone number:</label>
+
+                <input id="phoneNumber" placeholder="Phone Number" maxlength="16" />
+                `;
+            }
+            const inputElement = document.getElementById('phoneNumber');
+            inputElement.addEventListener('keydown', enforceFormat);
+            inputElement.addEventListener('keyup', formatToPhone);
+        }
 
 
         btnOpenModal.addEventListener('click', () => {
             modalBlock.classList.add('d-block');
             playTest(answerCount);
         });
-    
+
         closeModal.addEventListener('click', () => {
             modalBlock.classList.remove('d-block');
         });
-    
+
         nextButton.addEventListener('click', () => {
+            chechAnswer();
             formAnswers.innerHTML = '';
             answerCount++;
-            playTest(answerCount);
             answerCount >= 0 ? prevButton.classList.remove('hide') : null;
-            answerCount >= (questions.length-1) ? nextButton.classList.add('hide') : null;
-        })
+            if (answerCount >= questions.length) {
+                checkIsLast();
+                nextButton.classList.add('hide');
+                sendBtn.classList.remove('d-none');
+            }
+            playTest(answerCount);
+        });
         prevButton.addEventListener('click', () => {
+            chechAnswer();
             formAnswers.innerHTML = '';
             answerCount--;
-            playTest(answerCount);
+            
             answerCount <= 0 ? prevButton.classList.add('hide') : null;
-            answerCount <= (questions.length-1) ? nextButton.classList.remove('hide') : null;
-        })
+            if (answerCount <= questions.length) {
+                nextButton.classList.remove('hide');
+                sendBtn.classList.add('d-none');
+            }
+            playTest(answerCount);
+        });
+        sendBtn.addEventListener('click', () => {
+            obj[`phone number`] = phoneNumber;
+            chechAnswer();
+            modalBlock.classList.remove('d-block');
+            console.log(obj)
+        });
     });
- 
+
 });
